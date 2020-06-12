f6db09e4a16746004788c2070767f02044b7ea27
AbdulhamidAlelaiyan <quickisslow191@gmail.com>
2020-06-12 20:08:44 +0300

    Data can be loaded from server

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,1 +1,21 @@
-export const loadData
+import Vue from 'vue';
+
+export const loadData = ({ commit }) => {
+    Vue.http.get('data.json')
+        .then(response => response.json())
+        .then(data => {
+            if(data) {
+                const stocks = data.stocks;
+                const funds =  data.funds;
+                const stockPortolio = data.stockPortfolio;
+
+                const portfolio = {
+                    stockPortolio,
+                    funds,
+                };
+
+                commit('SET_STOCKS', stocks);
+                commit('SET_PORTFOLIO', portfolio)
+            }
+        });
+};
--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,8 @@ import Vuex from 'vuex';
 import stocks from './modules/stocks.js';
 import portfolio from './modules/portfolio.js';
 
+import { loadData } from './actions.js';
+
 
 Vue.use(Vuex);
 
@@ -11,5 +13,8 @@ export default new Vuex.Store({
     modules: {
         stocks,
         portfolio,
+    },
+    actions: {
+        loadData,
     },
 });
